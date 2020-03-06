f3d12c578a8b4e281754606cc9e110eb083a60f6
Brian Shanley <brian.shanley@wipro.com>
2020-03-06 13:40:20 +0000

    first push :)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import logo from './assets/labour-hopscotch.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </header>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Labour Hopscotch</p>
+
+      <Link to="/about">About</Link>
     </div>
   );
 }
 
+function About() {
+  return (
+    <div>
+      <p>About Labour Hopscotch</p>
+
+      <Link to="/">Home</Link>
+    </div>
+  );
+}
 export default App;
