d474cb60d1ce7182085585d6b4c613f1a310b972
Brian Shanley <brian.shanley@wipro.com>
2020-03-12 22:27:32 +0000

    some initial about content

--- src/App.js
+++ src/App.js
@@ -35,8 +35,23 @@ function Home() {
 
 function About() {
   return (
-    <div>
+    <div className="about-content">
       <p>About Labour Hopscotch</p>
+      <p>
+        The National Maternity hospital is making strides towards evidence based
+        care with it’s award winning innovative ‘Labour Hopscotch’ created by a
+        forward thinking Midwife Sinead Thompson. Mobility in labour has been
+        shown in study after study to shorten labour, reduce fetal distress and
+        reduce the need for medication as well as improving mum’s feelings of
+        control and satisfaction.
+      </p>
+      <p>
+        Labour Hopscotch – make each step count! A visual birthing tool designed
+        to aid you in an active birth. Providing structured guidance by
+        outlining 20-minute rotating “steps” to perform during labour. These
+        include keeping mobile by walking sideways on a stairs, or sitting on a
+        stool while being massaged.
+      </p>
       <p>
         Contact&nbsp;
         <a href="mailto:sinead@labourhopscotch.ie?subject=websitequery">
