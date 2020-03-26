5bd55cfce7d6774d75bf4d6fa768aefe67d731b4
Brian Shanley <brian.shanley@wipro.com>
2020-03-26 16:02:22 +0000

    contentful api integration

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter as HashRouter,
   Switch,
@@ -8,8 +8,27 @@ import {
 import logo from './assets/labour-hopscotch.jpg';
 import Steps from './pages/steps.js';
 import './App.scss';
+import * as contentful from 'contentful';
 
 function App() {
+  const [entries, setEntries] = useState([]);
+  useEffect(() => {
+    console.log('ineeeer');
+
+    const client = contentful.createClient({
+      space: '3mvtf9hx44sd',
+      accessToken: 'lidYYf2UG2FiinMVzNMJ3xcJq-4JMZfE5sTLoytxg20'
+    });
+    client.getEntries().then(entries => {
+      console.log('entries:', entries);
+      setEntries(entries.items);
+      entries.items.forEach(entry => {
+        if (entry.fields.title) {
+          console.log(entry.fields.title);
+        }
+      });
+    });
+  }, []);
   return (
     <HashRouter>
       <div className="App">
@@ -23,7 +42,15 @@ function App() {
             </div>
           </div>
           <div className="nav-steps">
-            <div className="nav-steps-item">
+            {entries.map(item => {
+              const linkUrl = '/steps/' + item.fields.urlTitle;
+              return (
+                <div className="nav-steps-item">
+                  <Link to={linkUrl}>{item.fields.title}</Link>
+                </div>
+              );
+            })}
+            {/* <div className="nav-steps-item">
               <Link to="/steps/alternative-therapy">Alternative Therapy</Link>
             </div>
             <div className="nav-steps-item">
@@ -34,7 +61,7 @@ function App() {
             </div>
             <div className="nav-steps-item">
               <Link to="/steps/water">Water</Link>
-            </div>
+            </div> */}
           </div>
         </header>
         <div class="app-body">
