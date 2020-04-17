b9deb73b4f232e9fa5eb7b9b1ff34759de66e6fa
brianshano <bshanley@gmail.com>
2020-04-17 22:29:53 +0100

    new responsive hopscotch board

--- src/pages/steps.js
+++ src/pages/steps.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './steps.scss';
 import * as contentful from 'contentful';
 
-const Steps = location => {
+const Steps = (location) => {
   const [entries, setEntries] = useState([]);
   useEffect(() => {
     var parts = window.location.pathname.split('/');
@@ -10,11 +10,11 @@ const Steps = location => {
     console.log('ineeeer', result);
     const client = contentful.createClient({
       space: '3mvtf9hx44sd',
-      accessToken: 'lidYYf2UG2FiinMVzNMJ3xcJq-4JMZfE5sTLoytxg20'
+      accessToken: 'lidYYf2UG2FiinMVzNMJ3xcJq-4JMZfE5sTLoytxg20',
     });
-    client.getEntries().then(entries => {
+    client.getEntries().then((entries) => {
       setEntries(
-        entries.items.filter(entry => {
+        entries.items.filter((entry) => {
           return result === entry.fields.urlTitle;
         })
       );
@@ -25,16 +25,16 @@ const Steps = location => {
     <>
       <div className="steps-container">{/* <Renderer id={id} /> */}</div>
       <div className="actions-container">
-        {entries.map(entry => {
+        {entries.map((entry) => {
+          console.log('loop', entry);
           return (
             <>
               <div className="action">
                 <img
-                  src={entry.fields.icon.fields.file.url}
+                  src={entry.fields.imageurl}
                   className="action-image"
                   alt={entry.fields.title}
                 />
-                {entry.fields.title}
               </div>
 
               <div className="action-description">
@@ -43,7 +43,7 @@ const Steps = location => {
                   if (type === 'paragraph') {
                     return <p>{content.content[0].value}</p>;
                   } else if (type === 'unordered-list') {
-                    return content.content.map(c => {
+                    return content.content.map((c) => {
                       return (
                         <div className="list-item">
                           {c.content[0].content[0].value}
