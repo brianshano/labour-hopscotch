16f3cf4a3cac82ea396a0f87a101a30f66a1d549
brianshano <bshanley@gmail.com>
2020-04-20 15:49:16 +0100

    use react formatting for contentful

--- src/components/ContentfulClient.js
+++ src/components/ContentfulClient.js
@@ -5,17 +5,18 @@ export default class ContentfulClient {
   getClient = async () => {
     const client = await createClient({
       space: '3mvtf9hx44sd',
-      accessToken: 'lidYYf2UG2FiinMVzNMJ3xcJq'
+      accessToken: 'lidYYf2UG2FiinMVzNMJ3xcJq',
     });
-    console.log('client', client);
 
     return client;
   };
-  getEntry = async id => {
+  getEntry = async (id) => {
     const client = await this.getClient();
-    const entry = await client.getEntry(id, { include: 10 }).then(response => {
-      console.log('here is response', response);
-    });
+    const entry = await client
+      .getEntry(id, { include: 10 })
+      .then((response) => {
+        console.log('here is response', response);
+      });
     // return entry;
   };
 }
